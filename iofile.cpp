2e800b8d6f5bc89cde5b27aa080ff804026997eb
MalkingMan <arrayalkhozini@gmail.com>
2024-06-04 14:37:31 +0700

    first commit

---
+++ iofile.cpp
@@ -0,0 +1,19 @@
+#include <fstream>
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    string baris;
+	
+    // Mmebuka file dalam mode menulis.
+	ofstream outfile;
+    // Menunjuk ke sebuah nama file
+    outfile.open("contohfile.txt");
+
+    cout << ">= Menulis file, \'q\' untuk keluar" << endl;
+    
+	
+}
