8485f9b3e07913aaf6dda8a7f37833e4c4fb33a7
MalkingMan <arrayalkhozini@gmail.com>
2024-06-04 15:21:09 +0700

    commit 10

--- ManualException.cpp
+++ ManualException.cpp
@@ -12,7 +12,14 @@ int main()
     }
     catch(int a)
     {
-        std::cerr << e.what() << '\n';
+        //Blok ini akan dieksekusi
+        cout << "Pengecualian akan dieksekusi" << endl;
     }
+    catch (...)
+    {
+        /*Jika selain integer maka blok ini akan dieksekusi*/
+        cout << "Default pengecualian dieksekusi" << endl;
+    }
+    return 0;
     
 }
