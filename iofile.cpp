3201bb289fbe5d7abe3cbc89e405b722396c469f
MalkingMan <arrayalkhozini@gmail.com>
2024-06-04 14:52:18 +0700

    commit 4

--- iofile.cpp
+++ iofile.cpp
@@ -33,5 +33,22 @@ int main()
     ifstream infile;
     //Menunjuk ke sebuah file
 	infile.open("contohfile.txt");
+
+    cout << endl << ">= Mmebuka dan membaca file" << endl;
+    //Jika file ada maka
+    if (inflie.is_open())
+    {
+        //Melakukan perulangan setiap baris
+		while (getline(infile, baris))
+        {
+            //Dan tampilkan disini
+            cout << baris << '/n';
+        }
+        //Tutup file tersebut setelah selesai
+        infile.close();
+    }
+    //Jika tidak ditemukan file maka akan menampilkan ini
+	else cout << "Unable to open file";
+	return 0;
     
 }
